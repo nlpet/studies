1b6dc326a917355750a99dc327dc977d64f2cc2e
Nora Petrova <schottkey@gmail.com>
2015-11-26 22:09:16 +0000

    Update qm.py

--- maths/qm.py
+++ maths/qm.py
@@ -2,8 +2,8 @@
 
 from collections import Counter
 
-# Expected value
-def expected_value(lst):
+# Mean value
+def mean_value(lst):
     c = Counter(lst)
     n = len(lst)
     
@@ -13,3 +13,12 @@ def expected_value(lst):
     		zip(c.keys(),c.values())
     	)
     )
+
+def expected_value(lst, probs):
+    prod = lambda t : t[0] * t[1]
+    return sum(map(prod, zip(lst, probs)))
+
+
+if __name__ == '__main__':
+    ev = expected_value(range(1,7),[1/6]*6)
+    print("Expected value of a 4-sided die is {}".format(ev))
