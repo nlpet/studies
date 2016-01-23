d8afc70b89e65d711172aabc82542a002b5818c9
Nora Petrova <schottkey@gmail.com>
2016-01-23 21:20:20 +0000

    Update modelling_potential_energy.py

--- maths/modelling_potential_energy.py
+++ maths/modelling_potential_energy.py
@@ -2,7 +2,7 @@ from numpy.polynomial import Polynomial
 from math import fabs
 
 
-def find_rs(coefficients):
+def find_roots(coefficients):
     p = Polynomial(coefficients)
     print "\nRoots of U(x) with E = %d are:" % fabs(coefficients[0])
     for i, r in enumerate(p.roots()):
@@ -15,7 +15,7 @@ def find_rs(coefficients):
 
 if __name__ == '__main__':
     # Passing the coefficients of U(x) for E = 5
-    find_rs([-5, 0, 6, -1/2., -1])
+    find_roots([-5, 0, 6, -1/2., -1])
 
     # Passing the coefficients of U(x) for E = 15
-    find_rs([-15, 0, 6, -1/2., -1])
+    find_roots([-15, 0, 6, -1/2., -1])
